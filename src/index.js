8084278e89dde0ff5665c00b28e50652891c6f4e
LittleDC <1187061276@qq.com>
2021-07-15 18:08:33 +0800

    css practice done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css'
 
 function component() {
   const element = document.createElement('div');
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
