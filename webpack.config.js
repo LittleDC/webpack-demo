6d412a04ab2759469ac7a055797019f718c6b4a4
LittleDC <1187061276@qq.com>
2021-07-19 15:35:43 +0800

    develop-serve practice

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      title: 'Development',
     }),
   ],
   output: {
