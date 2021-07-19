8eace1092f874a4b5b408ce1101f3aa87fae7ce7
LittleDC <1187061276@qq.com>
2021-07-19 11:13:25 +0800

    image practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
