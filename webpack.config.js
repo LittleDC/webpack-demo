7a71b875193149910d8f415bb517f82d51fd2df2
LittleDC <1187061276@qq.com>
2021-07-19 15:14:18 +0800

    output practice

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    // path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/dist',
+    clean: true,
   },
 };
