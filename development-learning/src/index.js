9cc0daee16b954c1fde70141683f0f27fc7cb244
Dai Chen <48859961+LittleDC@users.noreply.github.com>
2021-08-27 23:23:35 +0800

    development-learning & bug appear

---
+++ development-learning/src/index.js
@@ -0,0 +1,8 @@
+// 这是入口文件
+import './index.less'
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(1, 2));
--- development-learning/webpack.config.js
+++ development-learning/webpack.config.js
@@ -1,3 +1,66 @@
-module.exports={
-  
+/*
+  开发环境配置：能让代码运行起来
+
+*/
+
+const {
+  resolve
+} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      // loader的配置
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          // 关闭es6模块化，开启commonJS模块化
+          esModule: false
+        }
+      },
+      {
+        // 处理HTML中的img资源
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        // 处理其他资源
+        exclude: /\.(html|js|css|less|jpg|png|gif)/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // plugins的配置
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    open: true
+  },
+  mode: 'development'
 }
